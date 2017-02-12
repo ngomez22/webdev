af736e832e5fdd6a793594d3e032adc6f75ef9b4
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-02-12 12:16:06 -0500

    Implement sidebar scroll functionality

--- my-website/js/sketch.js
+++ my-website/js/sketch.js
@@ -1,4 +1,13 @@
 (function setup() {
+  //Setup sidebar links
+  var sidebarLinks = document.getElementsByClassName("sidebar-link");
+  for (var i = 0; i < sidebarLinks.length; i++) {
+    var href = sidebarLinks[i].getAttribute("href").replace(/#/g, "");
+    var target = document.getElementById(href);
+    sidebarLinks[i].addEventListener("click", function(event) {
+      target.scrollIntoView(true);
+    })
+  }
   //Setup social media buttons highlight functionality
   var buttons = document.getElementsByClassName("social-media");
   for (var i = 0; i < buttons.length; i++) {
