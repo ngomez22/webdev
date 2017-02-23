2ce5721c5210a9befb6c6aa9db2ed613533b8256
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-02-22 22:23:16 -0500

    Copy node class list files into mongo folder

--- mongo-tweets/routes/index.js
+++ mongo-tweets/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var tweets = require('../public/javascripts/tweets');
+var students = require(__dirname + '/tweets');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { list : students });
+});
+
+/* GET tweets */
+router.get('/tweets', function(req, res, next) {
+  res.json(tweets);
+})
+
+/* GET students */
+router.get('/students', function(req, res, next) {
+  res.json(students);
 });
 
 module.exports = router;
---
+++ mongo-tweets/routes/tweets.js
@@ -0,0 +1,23 @@
+var tweets = require('../public/javascripts/tweets');
+
+function load() {
+  var students = {};
+  for(var i=0; i<tweets.length; i++) {
+    var name = tweets[i].user.name;
+    var text = tweets[i].text;
+    if(!students[name] && !tweets[i].retweeted_status) {
+      students[name] = {
+        "name" : name,
+        "image" : tweets[i].user.profile_image_url,
+        "handle" : tweets[i].user.screen_name,
+        "tweets" : []
+      };
+    }
+    if(!tweets[i].retweeted_status) {
+      students[name].tweets.push(text);
+    }
+  }
+  return students;
+}
+
+module.exports = load();
